0b1125f7d6810cc706ab3febb5ed3df98a2a2a9b
Zachary Nelson <nelsonz2013@hotmail.com>
2020-10-24 13:35:12 -0500

    Added redux devtools support

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
-import redux, { createStore } from 'redux';
+import redux, { applyMiddleware, createStore } from 'redux';
 import globalStatReducer from '../reducers/globalStatReducer.js';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(globalStatReducer);
+const store = createStore(globalStatReducer, composeWithDevTools());
 
 export default store;
